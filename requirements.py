7f478d0ae3cf15f2670686698660f7de276b64a9
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2014-10-05 15:23:55 -0500

    update of public version

--- requirements.py
+++ requirements.py
@@ -8,10 +8,9 @@ import os
 
 
 
-packages = ['gfortran', 'python3-dev', 'python3-numpy', 'python3-scipy', 'python3-pandas',
-				'python3-setuptools', 'python3-pip', 'python3-nose', 'git']
+packages = ['gfortran', 'python3-dev', 'python3-numpy', 'python3-scipy']
 
 
-#for package in packages:
+for package in packages:
 
-#    os.system('sudo apt-get install -y ' + package)
+    os.system('sudo apt-get install -y ' + package)
