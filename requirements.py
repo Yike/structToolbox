0eaca14f283e852739cbdba433fac196343541da
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2014-10-05 14:48:48 -0500

    update of public version

---
+++ requirements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+''' This script provisions the required software environment to run
+	the structToolbox.
+'''
+
+# standard library
+import os
+
+
+
+packages = ['gfortran', 'python3-dev', 'python3-numpy', 'python3-scipy', 'python3-pandas',
+				'python3-setuptools', 'python3-pip', 'python3-nose', 'git']
+
+
+for package in packages:
+
+    os.system('sudo apt-get install -y ' + package)
