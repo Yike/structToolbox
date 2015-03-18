bafdaf208707409a1c509ef5936fe2c70a52e735
Yike <ywang18@uchicago.edu>
2015-03-17 22:09:25 -0500

    test simulate

---
+++ scripts/tests_scripts/test_simulate.py
@@ -0,0 +1,18 @@
+"""
+Filename: test_simulate.py
+Tests for simulate.py file.
+"""
+
+# standard library
+import unittest
+
+from scripts.simulate import simulate
+
+class TestSimulate(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
